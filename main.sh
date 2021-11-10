e433d0c2aa969f5c9f8e17018ae33550ac05f729
Johanna Fericean <71076130+johakespeare@users.noreply.github.com>
2021-11-10 13:59:58 +0100

    ajout de la fonction afficherImages
    Co-Authored-By: Ghinevra <71076162+ghinc@users.noreply.github.com>

--- main.sh
+++ main.sh
@@ -9,19 +9,30 @@ cat > test.html << EOF
     <title>TP admin système</title>
   </head>
   <body>
-    <h1>Les superbes images !!</h1>
-    <img      src="images/cafe.jpeg"      alt="cafe">
-     <img      src="images/capybara.jpeg"      alt="capybara">
-      <img      src="images/corse.png"      alt="corse">
-       <img      src="images/poney.jpeg"     alt="poney">
-    
-    
-    
+    <h1>Les superbes images !!</h1> 
+   
+  
+
   </body>
 </html>
 
 
 EOF
+
+
+function afficherImages(){
+	images=$(ls images)
+
+	for image in $images
+	do
+	
+	echo '<img src="images/'$image'" alt="'$image'">'
+	
+	done	
+}
+
+
+afficherImages >>test.html
 
 xdg-open "test.html"&
 
