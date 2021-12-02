079169c9921800462895a268cb8e1c369c00745b
Johanna Fericean <71076130+johakespeare@users.noreply.github.com>
2021-12-02 13:06:16 +0100

    Update main.sh
    ajout de fonctions, gestion d'erreurs, ajout de paramètres
    
    Co-Authored-By: Ghinevra <71076162+Ghinc@users.noreply.github.com>

--- main.sh
+++ main.sh
@@ -1,38 +1,126 @@
 #!/bin/bash
-touch test.html
-cat > test.html << EOF
-
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>TP admin système</title>
-  </head>
-  <body>
-    <h1>Les superbes images !!</h1> 
-  </body>
-</html>
 
 
-EOF
+PAGE_HTML=index.html
+PATH_IMAGES=""
 
 
-function afficherImages(){
-	images=$(ls images)
+function createHtml()
+{
+    touch $PAGE_HTML
+    initHtml    
+}
+
+function initHtml()
+{
+	echo "<!DOCTYPE html>
+	<html>
+  	<head>
+    		<title>TP admin système</title>
+  	</head>
+  		<body>
+    		<h1>Les superbes images !!</h1> 
+  		</body>
+	</html>" >> $PAGE_HTML
 	
-	if [ -z "$images" ]
-	then
-		echo "<p> il n'y a pas d'image </p>"
+}
+
+function display()
+{
+	if ! [ -f "$PAGE_HTML" ] ; then
+	createHtml
 	fi
-	
-	for image in $images
-	do
-		echo '<img src="images/'$image'" alt="'$image'">'
-	done	
+	afficherImages >> $PAGE_HTML
+	xdg-open "$PAGE_HTML"&
 }
 
 
-afficherImages >>test.html
+function afficherImages()
+{
+	
+	$images = ls PATH_IMAGES
+	if [ -z $images  ]
+	then
+		echo "<p> il n'y a pas d'image </p>"
+	else		
+		for image in PATH_IMAGES"/"*;do
+			echo '<img src="'$image'" alt="'$image'">'
+		done	
+	fi
+}
 
-xdg-open "test.html"&
+
+function error()
+{
+	echo "ERREUR : Mauvais paramètres!" >&2
+	echo "Veuillez consulter l'option --help pour plus d'informations" >&2
+
+}
+
+
+
+function build()
+{	
+	if ! [ -z "$1" ] ; then
+		if ! [ -d "$1" ] ; then	
+			mkdir $1	
+		fi
+		#mv images $1
+		#cd "$1"
+		#$PATH_IMAGES=$1images
+		#createHtml
+				
+	else
+		echo "il manque un paramètre" >&2
+		
+	fi
+
+}
+
+
+
+
+
+
+if [ "$1" = "--help" ] ; then
+	echo "AIDE">&2
+	exit 0
+fi
+if [ "$1" = "build" ] ; then
+	build $2
+	exit 0
+fi
+
+if [ "$1" = "display" ] ; then
+	display 
+	exit 0
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
