b73f7f6da0cc68041ac55f0a14d8b23a70d60a2e
Johanna Fericean <71076130+johakespeare@users.noreply.github.com>
2021-12-05 13:59:58 +0100

    ajout authentification
    ajout d'un fichier csv users.csv comme base de données,
    ajout de la fonction register et authentificate avec les fonctions de vérifications associées.
    
    Co-Authored-By: Ghinevra <71076162+Ghinc@users.noreply.github.com>

--- main.sh
+++ main.sh
@@ -3,6 +3,7 @@
 
 PAGE_HTML=index.html
 PATH_IMAGES=""
+PATH_DB=database
 
 
 function createHtml()
@@ -65,7 +66,7 @@ function build()
 		if ! [ -d "$1" ] ; then	
 			mkdir $1	
 		fi
-		#mv images $1
+		#cp -r  -t$1 ../tp-admin_sys 
 		#cd "$1"
 		#$PATH_IMAGES=$1images
 		#createHtml
@@ -77,8 +78,95 @@ function build()
 
 }
 
+function register()
+{
+
+	read -p "Entrez le nom d'utilisateur : " USER_NAME 
+	
+	isBlank "$USER_NAME" "nom d'utilisateur"
+	until [ $(isRegistered "$USER_NAME") -eq 0 ]
+	do
+		echo "Le nom d'utilisateur $USER_NAME est déjà pris"
+		read -p "Entrez le nom d'utilisateur : " USER_NAME 
+		isBlank "$USER_NAME" "nom d'utilisateur"		
+	done
+	
+	read -sp "Entrez le mot de passe : " USER_PASSWORD_1
+	isBlank "$USER_PASSWORD_1" "mot de passe"
+
+	read -sp "Entrez de nouveau le mot de passe : " USER_PASSWORD_2
+	
+	until [ $USER_PASSWORD_1 = $USER_PASSWORD_2 ]
+	do	
+		echo "les mots de passes sont différents">&2
+		read -sp "Entrez le mot de passe : " USER_PASSWORD_1
+		isBlank "$USER_PASSWORD_1" "mot de passe"
+		read -sp "Entrez de nouveau le mot de passe : " USER_PASSWORD_2
+			
+	done
+	echo $USER_NAME $USER_PASSWORD_1 >> "$PATH_DB"/users.csv
+	
+}
+
+function isBlank()
+{
+ 	if [ -z $1 ] ; then
+	    echo "Le $2 ne peut pas être vide !" 
+	    exit 0 
+	fi 
+
+}
+
+function isRegistered()
+{
+
+	res=0
+	while read -r username password
+	do	  
+	  if [ "$username" = $1 ] ; then
+	  	res=1
+	  fi	  
+	done < "$PATH_DB"/users.csv
+	echo $res	
+}
 
 
+function authenticate()
+{
+
+	read -p "Entrez le nom d'utilisateur : " USER_NAME 
+	isBlank "$USER_NAME" "nom d'utilisateur"
+	until [ $(isRegistered "$USER_NAME") -eq 1 ]
+	do
+		echo "nom d'utilisateur $USER_NAME inconnu"
+		read -p "Entrez le nom d'utilisateur : " USER_NAME 
+	done
+	read -p "Entrez le mot de passe : " USER_PASSWORD
+	isBlank "$USER_PASSWORD_1" "mot de passe"
+	until [ $USER_PASSWORD = $(getMdp "$USER_NAME") ]
+	do	
+		echo "le mot de passe est incorrect ">&2
+		read -p "Entrez le mot de passe : " USER_PASSWORD
+		isBlank "$USER_PASSWORD_1" "mot de passe"
+				
+	done	
+
+
+}
+
+function getMdp()
+{
+	res='' 
+	while read -r username password
+	do
+	 
+	  if [ $username = $1 ] ; then
+	  	res=$password
+	  fi
+	  
+	done < "$PATH_DB"/users.csv
+	echo $res
+}
 
 
 
@@ -96,9 +184,22 @@ if [ "$1" = "display" ] ; then
 	exit 0
 fi
 
+if [ "$1" = "register" ] ; then
+	register
+	exit 0
+fi
+
+if [ "$1" = "authenticate" ] ; then
+	authenticate
+	exit 0
+fi
 
 
 
+if [ "$1" = "mdp" ] ; then
+	getMdp $2
+	exit 0
+fi
 
 
 
